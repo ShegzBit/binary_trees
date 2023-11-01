cededc5a93403b289d4c7831807ded3b67cc0d07
ShegzBit <feranmiawotubo16@gmail.com>
2023-11-01 22:19:38 +0100

    solved task 111 again

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -34,6 +34,11 @@ bst_t *bst_insert(bst_t **tree, int value)
 		temp2->left = node;
 	else if (node->n > temp2->n)
 		temp2->right = node;
+	else
+	{
+		free(node);
+		return (NULL);
+	}
 
 	return (node);
 }
