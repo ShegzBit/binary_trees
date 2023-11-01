8362302c5a19d539edf5f02deffdc0676948ff22
ShegzBit <feranmiawotubo16@gmail.com>
2023-11-01 15:09:25 +0100

    solved task 110

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,4 +1,8 @@
 #include "binary_trees.h"
+#include <limits.h>
+#define RIGHT tree->right
+#define LEFT tree->left
+#define TN tree->n
 
 /**
  * binary_tree_is_bst - checks if a tree is a bst
@@ -7,6 +11,23 @@
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	(void)tree;
-	return (0);
+	if (!tree)
+		return (0);
+	return (bst_helper(tree, INT_MIN, INT_MAX));
+}
+
+/**
+ * bst_helper - helps binray tree in recursive call int limits
+ * @tree: tree to work on
+ * @min: minimum number
+ * @max: maximum number
+ * Return: 1 is bst | 0 not bst
+ */
+int bst_helper(const bst_t *tree, int min, int max)
+{
+	if (!tree)
+		return (1);
+	if (min >= TN || max <= TN)
+		return (0);
+	return (bst_helper(LEFT, min, TN - 1) && bst_helper(RIGHT, TN + 1, max));
 }
