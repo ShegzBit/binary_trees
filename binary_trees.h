94d578d395a543d7e1c939facd96350d2858a80c
ShegzBit <feranmiawotubo16@gmail.com>
2023-11-01 11:55:22 +0100

    Solved task 102

--- binary_trees.h
+++ binary_trees.h
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #ifndef _BINARY_TREES_
 #define _BINARY_TREES_
+
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -13,10 +13,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -31,7 +31,7 @@ typedef binary_tree_t bt_t;
 void binary_tree_print(const bt_t *parent);
 int height_bt(const binary_tree_t *tree);
 size_t size_bt(const binary_tree_t *tree);
-int is_left (const binary_tree_t *tree);
+int is_left(const binary_tree_t *tree);
 int complete_helper(const binary_tree_t *tree, size_t height);
 
 /*Prototypes*/
@@ -56,7 +56,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second);
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
