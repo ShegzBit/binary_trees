225723182082d97aa266bd4d32e4aa7a8b4fabbc
ShegzBit <feranmiawotubo16@gmail.com>
2023-11-03 02:30:03 +0100

    added 131 prototype to header file

--- binary_trees.h
+++ binary_trees.h
@@ -78,5 +78,6 @@ int binary_tree_is_avl(const binary_tree_t *tree);
 avl_t *avl_insert(avl_t **tree, int value);
 avl_t *avl_remove(avl_t *root, int value);
 int binary_tree_is_heap(const binary_tree_t *tree);
+int binary_tree_is_avl(const binary_tree_t *tree);
 
 #endif /**/
