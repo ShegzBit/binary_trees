79f727e3a9a5f8cea7c7acbbed209e5433fa634f
ShegzBit <feranmiawotubo16@gmail.com>
2023-11-03 03:23:54 +0100

    added template for task 134

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - heap to sorted arrays
+ * @heap: heap to convert
+ * @size: predetermind size of array
+ * Return: pointer to the array
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	(void)heap;
+	(void)size;
+	return (NULL);
+}
